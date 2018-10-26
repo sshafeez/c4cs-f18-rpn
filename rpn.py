22d9041ea6f70bd89ffb1db7b4a5523e4dd6aefd
sshafeez <sshafeez@umich.edu>
2018-10-26 11:17:21 -0400

    passed first addition test

--- rpn.py
+++ rpn.py
@@ -1,5 +1,19 @@
+#!/usr/bin/env python3
+
 def calculate(arg):
     pass
+    stack = []
+    tokens = arg.split()
+    for token in tokens:
+        try:
+            value = int(token)
+            stack.append(value)
+        except ValueError:
+            val1 = stack.pop()
+            val2 = stack.pop()
+            result = val1 + val2
+            stack.append(result)
+            return stack[0]
 def main():
     while True:
         calculate(input('rpn calc>'))
