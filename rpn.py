670175f47040e6d37bbfd1bcb9dd36466597fb63
sshafeez <sshafeez@umich.edu>
2018-10-26 11:33:12 -0400

    mult/div features

--- rpn.py
+++ rpn.py
@@ -15,6 +15,10 @@ def calculate(arg):
                 result = val1 + val2
             elif token == '-':
                 result = val1 - val2
+            elif token == '*':
+                result = val1 * val2
+            elif token == '/':
+                result = val1 / val2
             stack.append(result)
             return stack[0]
 def main():
